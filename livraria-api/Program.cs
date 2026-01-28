2c8dda68ff90f30a68e506dc45562293a9cbf4a1
Bruno Calera <brunocalerasp@gmail.com>
2026-01-28 20:52:09 -0300

    feat: Adicionando Swagger a API

--- livraria-api/Program.cs
+++ livraria-api/Program.cs
@@ -3,10 +3,22 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minha API v1");
+        c.RoutePrefix = string.Empty; // Swagger na raiz, ex: http://localhost:5000/
+    });
+}
 
 app.UseHttpsRedirection();
 
